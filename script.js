42235a7ad0a573925a3a8e88709515a21e745d69
ginuel <xpsl4b@gmail.com>
2026-07-28 18:40:17 +0800

    add buttons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 let score = 0;
 
 const display = document.querySelector('#display');
+
+const incrementBtn = document.querySelector("#increment");
+const decrementBtn = document.querySelector("#decrement");
+const resetBtn = document.querySelector("#reset");
+
+incrementBtn.addEventListener('click', () => {
+	score++;
+	display.textContent = `Score: ${score}`;
+});
+
+decrementBtn.addEventListener('click', () => {
+	score--;
+	display.textContent = `Score: ${score}`;
+});
+
+resetBtn.addEventListener('click', () => {
+	score = 0;
+	display.textContent = `Score: ${score}`;
+});

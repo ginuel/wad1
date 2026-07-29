5120d1ba1c0093557533358fb540a294760ec417
ginuel <xpsl4b@gmail.com>
2026-07-29 12:51:20 +0800

    add 3 buttons

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 let score = 0;
+
+const display = document.quertSelector('#display');
+const incrementBtn = document.quertSelector('#increment');
+const decrementBtn = document.quertSelector('#decrement');
+const resetBtn = document.quertSelector('#reset');
+
+incrementBtn.addEventListener('click', () => {
+	score++;
+	display.textContent = `Score: ${score}`;
+});
+
+decrementBtn.addEventListener('click', () => {
+	score--;
+	display.textContent = `Score: ${score}`;
+});
+
+resetBtn.addEventListener('click', () => {
+	score = 0;
+	display.textContent = `Score: ${score}`;
+});
